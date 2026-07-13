758b4659bbf6c8fde13d3a668038c488df7c1c91
Ahmed Abelhamid <ahmedabdelhamid360356@gmail.com>
2026-07-13 22:50:45 +0300

    Refactor code structure for improved readability and maintainability and  finish historical periods page UI and functionality

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,23 @@
+// ============up to top for body=========== //
+let btn_up_body = document.getElementById("btn-up_body")
+
+window.addEventListener("scroll",function(){
+  
+  window.scrollY > 200 && window.scrollY < 3500? btn_up_body.classList.add("show") : btn_up_body.classList.remove("show") 
+  
+})
+
+if(btn_up_body){
+btn_up_body.addEventListener("click",function(){
+       
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+})
+}
+
+
 (function egyptSymbols(){
     let svg = document.getElementById("egypt-bg");
     let W = window.innerWidth;
@@ -5,26 +25,30 @@
     svg.setAttribute("viewBox", `0 0 ${W} ${H}`);
 
     let symbols = ["☥","𓅓","𓂀","𓆣","𓇼"];
-    let anmations = ["dw-up", "up-dw"]
     let html = "";
 
-    for(let i = 0; i < 20; i++){
+    for(let i = 0; i < 50; i++){
 
         let x = Math.random() * W;
         let y = Math.random() * H;
-        let size = Math.random() * 20 + 10;
+        let size = Math.random() * 1 + 0.1;
+        let time = Math.random()* 20 + 10
 
-        let symbol = symbols[Math.floor(Math.random()*symbols.length)];
-        let anmation = anmations[Math.floor(Math.random()*anmations.length)]
-
+        // let symbol = symbols[Math.floor(Math.random()*symbols.length)];
+        // <text x="${x}" y="${y}" font-size="${size}"fill="#C9A84C"opacity="0.3" style="animation: id ${time}s ease infinite;">${symbol}</text>
         html += `
-        <text x="${x}" y="${y}" font-size="${size}"fill="#C9A84C"opacity="0.3" style="animation: ${anmation} 10s ease infinite;">${symbol}</text>
+        <circle cx="${x}" cy="${y}" r="${size}" fill="#C9A84C" opacity="0.3" style="animation: id ${time}s ease infinite;"/>
         `;
     }
 
     svg.innerHTML = html;
 
 })();
+
+//main page
+
+
+
 
 let cards = document.querySelectorAll(".card-animation")
 
@@ -51,6 +75,178 @@ let observer = new IntersectionObserver(function(entries){
 cards.forEach(function(card , index){
     observer.observe(card)
 });
+
+
+
+//eras page
+
+let x = new XMLHttpRequest();
+let btn = document.getElementById("btn")
+
+x.open("GET","../DATA/Eras.json")
+
+x.send();
+
+x.addEventListener("load",function(){
+
+    if(x.status >= 200 && x.status < 300){
+        console.log(JSON.parse(x.response))
+
+        let eras = JSON.parse(x.response)
+        let cards = document.getElementById("cards")
+        let cartona = ""
+
+        for(let i = 0; i < eras.length; i++ ){
+          
+            if(i % 2 == 0){
+                  cartona += `
+            <div class="timeline-item right position-relative ">
+            <div class="data data-right position-absolute w-25 d-lg-flex d-none justify-content-start align-items-center gap-3">
+              <div class="dott right-dott ${eras[i].colorEra}"></div>
+               <div class="year w-50">
+                ${eras[i].startDate}
+              </div>
+            </div>
+            <div class="inner">
+                  <div class="card-content p-4 d-flex flex-column gap-2 justify-content-between">
+                    <div class="card-dtails d-flex justify-content-between align-items-center flex-wrap gap-sm-0 gap-3">
+                      <h2 class="m-0">${eras[i].name}</h2>
+                       <p class="m-0">${eras[i].startDate} ${eras[i].endDate}</p>
+                    </div>
+                    <h3>${eras[i].description}</h3>
+                    <div class="eras-dtails d-flex justify-content-between align-items-center flex-wrap gap-sm-0 gap-3">
+                        <div class="period-stats d-flex justify-content-center align-items-center gap-3 ">
+                          <h4>${eras[i].dynasties.length} اسرة حاكمة</h4>
+                          <h4>${eras[i].kings} ملوك</h4>
+                        </div>
+                        <a href="" class="link-underline link-underline-opacity-0 explore-btn text-center d-flex gap-2 align-items-center ">
+                          استكشف الأسرات
+                           <i class="fa-solid fa-arrow-left icon"></i>
+                        </a>
+                    </div>
+                  </div>
+            </div>
+             
+        </div>
+            
+          `
+            }else{
+                 cartona += `
+                     <div class="timeline-item left position-relative ">
+            <div class="data data-left position-absolute w-25 d-lg-flex d-none justify-content-end align-items-center gap-3 ">
+               <div class="year text-end w-50">
+                ${eras[i].startDate}
+              </div>
+              <div class="dott right-dott  ${eras[i].colorEra}"></div>
+            </div>
+            <div class="inner">
+                  <div class="card-content p-4 d-flex flex-column gap-2 justify-content-between">
+                    <div class="card-dtails d-flex justify-content-between align-items-center flex-wrap gap-sm-0 gap-3">
+                      <h2 class="m-0">${eras[i].name}</h2>
+                       <p class="m-0">${eras[i].startDate} - ${eras[i].endDate}</p>
+                    </div>
+                    <h3>${eras[i].description}</h3>
+                    <div class="eras-dtails d-flex justify-content-between align-items-center flex-wrap gap-sm-0 gap-3">
+                        <div class="period-stats d-flex justify-content-center align-items-center gap-3">
+                          <h4>${eras[i].dynasties.length} اسرة حاكمة</h4>
+                          <h4>${eras[i].kings} ملوك</h4>
+                        </div>
+                        <a href="" class="link-underline link-underline-opacity-0 explore-btn text-center d-flex gap-2 align-items-center">
+                          استكشف الأسرات
+                           <i class="fa-solid fa-arrow-left icon"></i>
+                        </a>
+                    </div>
+                  </div>
+            </div>
+             
+        </div>
+          
+          `
+            }
+
+         
+            
+        } 
+
+        if(cards){
+           cards.innerHTML= cartona;
+        } 
+
+       
+    }
+
+    let items = document.querySelectorAll(".timeline-item")
+
+let observer2 = new IntersectionObserver(function(entries){
+
+      console.log(entries)
+    
+        entries.forEach(function(entry){
+
+        if(entry.isIntersecting){
+           
+            entry.target.classList.add("show")
+            
+        }
+
+    });
+   
+
+});
+
+
+
+items.forEach(function(card){
+    observer2.observe(card)
+});
+
+ let timeline_items = document.querySelectorAll(".timeline-item")
+
+ timeline_items.forEach(function(item){
+    item.addEventListener("click",function(){
+   
+        let cruuntItem = item
+        timeline_items.forEach(function(item){
+            if(item !== cruuntItem){
+                item.classList.remove("active")
+            }
+        })
+
+        this.classList.toggle("active")
+    })
+ })
+
+//  scroll
+  
+
+ const timeline = document.querySelector(".inners-period");
+
+ if(timeline){
+   const fill = document.querySelector(".progress-fill");
+
+const eye = document.querySelector(".progress-eye");
+
+window.addEventListener("scroll",()=>{
+
+    const rect = timeline.getBoundingClientRect();
+
+    const total = timeline.offsetHeight;
+
+    let progress = (window.innerHeight - rect.top) / total;
+
+    progress = Math.max(0,Math.min(progress,1));
+
+    fill.style.width = `${progress*100}%`;
+
+    eye.style.left = `${progress*100}%`;
+
+});
+ }
+
+})
+
+
+
 
 
 // fetch("./DATA/Eras.json")
